bfc41a6fe0fe6a0036329c1344027310bb421ebe
Akash <akash.dvlpr@gmail.com>
2021-08-09 12:19:23 +0530

    function updated

--- library/src/main/java/com/akashdev/kotlin_extensions/String.kt
+++ library/src/main/java/com/akashdev/kotlin_extensions/String.kt
@@ -5,11 +5,27 @@ import android.webkit.URLUtil
 import androidx.core.text.trimmedLength
 import java.net.URLDecoder
 import java.util.*
+import java.util.regex.Pattern
+
 
 fun String.findAnyOfWithRegex(strings: List<String>): String? {
     return strings.find { string ->
-        if (string.contains(".*")) this.contains(string.toRegex()) else this.contains(string, true)
-    }?.removeSpecialChar()
+        val pattern = Pattern.compile(string)
+        val matcher = pattern.matcher(this)
+        val matchFound = matcher.find()
+        if (matchFound) {
+            return if (matcher.groupCount() == 0) {
+                string.replace(".*", " ")
+            } else {
+                var value = ""
+                val list = 1..matcher.groupCount()
+                for (i in list) {
+                    value += (if (list.last == i) matcher.group(i) else "${matcher.group(i)} ")
+                }
+                value
+            }
+        } else false
+    }
 }
 
 fun String.isValidEmail(): Boolean = isNotEmpty() && Patterns.EMAIL_ADDRESS.matcher(this).matches()
@@ -19,7 +35,7 @@ fun String.isValidURL() = URLUtil.isValidUrl(this) && Patterns.WEB_URL.matcher(t
 fun String.removeSpecialChar(removeSpaces: Boolean? = false): String {
     val str = replace("%(?![0-9a-fA-F]{2})".toRegex(), "")
     val decoded = URLDecoder.decode(str, "UTF-8")
-    val regex = "[\"\\]\\[\\\\\$*+-.,~_;`^':!?<>/|›#&@{}()]".toRegex()
+    val regex = "[\"\\]\\[\\\\\$-.,~_;`^':!?<>/|›&@{}()]".toRegex()//skip only -- *#+
 
     val replacementOfSpaces = if (removeSpaces == true) "" else " "
     return decoded.lowercase().replace(regex, "").replace("\\s+".toRegex(), replacementOfSpaces)
